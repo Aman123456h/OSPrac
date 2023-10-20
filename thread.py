b141afcdc0162b52c118ea7606ea717df7bbf691
Aman123456h <jaman0120@gmail.com>
2023-10-20 22:43:48 +0530

    Final

--- thread.py
+++ thread.py
@@ -35,5 +35,27 @@ th2.join()
 
 #fibonacci series in threading
 
+import threading as th
 
+def fibonacci(n):
+    if n<=0:
+        return[]
+    elif n==1:
+        return[0]
+    elif n==2:
+        return[0,1]
+    else:
+        fib_list = [0,1]
+        for i in range(2,n):
+            fib_list.append(fib_list[i-1]+fib_list[i-2])
+        return fib_list
+def calculate_fib(n):
+    result = fibonacci(n)
+    print(f"fibonacci series of {n} is {result}")
 
+if __name__ == "__main__":
+    n=int(input("enter the number of fibonacci terms  to generate:"))
+    t = th.Thread(target=calculate_fib,args=(n,))
+    t.start()
+    t.join()
+    print("Done!")
